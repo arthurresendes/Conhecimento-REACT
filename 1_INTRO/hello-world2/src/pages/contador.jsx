85af50866e70693f91a50c053d5f1be0068969ce
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-05-25 14:45:08 -0300

    Tooltip

--- 1_INTRO/hello-world2/src/pages/contador.jsx
+++ 1_INTRO/hello-world2/src/pages/contador.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react';
+import { Tooltip } from 'react-tooltip';
 
 function Contador(){
     const [contador, setContador] = useState(0)
     return(
         <div>
-            <button onClick={() => setContador(contador + 1)}>Adicionar</button>
+            <button data-tooltip-id="tooltip" data-tooltip-content="Clique aqui adicionar na contagem!" onClick={() => setContador(contador + 1)}>Adicionar</button>
 
             <br />
             <p>Contador = {contador}</p>
+
+            <Tooltip id="tooltip" place="right"/>
         </div>
     )
 }
