1beeef8da133f5a21316b325c32f1c2cd84b3224
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-05-30 11:53:37 -0300

    Fundamentos

---
+++ 2_COMPONENTS/fundamentos/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import FirstComponent from "./components/FirstComponents"
+
+function App() {
+  return (
+    <div style={{textAlign: 'center'}}>
+      <h1>Fundamentos React</h1>
+      <FirstComponent/>
+    </div>
+  )
+}
+
+export default App
---
+++ 2_COMPONENTS/fundamentos/src/components/FirstComponents.jsx
@@ -0,0 +1,13 @@
+// Importações
+
+const FirstComponent =() => {
+    return (
+        <div>
+            {/* Outro tipo de comentario */}
+            <h1>Meu componente</h1>
+            <p className="text">Meu texto</p>
+        </div>
+    )
+}
+
+export default FirstComponent;
