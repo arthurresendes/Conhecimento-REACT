b620bb3ced0f1503c2a980883bef45b5c69588c7
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-05-30 11:59:32 -0300

    Hierarquia

--- 2_COMPONENTS/fundamentos/src/components/FirstComponents.jsx
+++ 2_COMPONENTS/fundamentos/src/components/FirstComponents.jsx
@@ -1,11 +1,16 @@
 // Importações
 
+import MyComponents from "./MyComponents";
+import TemplateExpressions from "./TemplateExpression";
+
 const FirstComponent =() => {
     return (
         <div>
             {/* Outro tipo de comentario */}
             <h1>Meu componente</h1>
             <p className="text">Meu texto</p>
+            <TemplateExpressions />
+            <MyComponents/>
         </div>
     )
 }
---
+++ 2_COMPONENTS/fundamentos/src/components/MyComponents.jsx
@@ -0,0 +1,9 @@
+const MyComponents = () => {
+    return (
+        <div>
+            <h2>Sendo reaproveitado no código</h2>
+        </div>
+    )
+}
+
+export default MyComponents;
