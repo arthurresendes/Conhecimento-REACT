9d60532a07db38a4ae6cff97ac779ad4a7314f41
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-06-02 14:10:33 -0300

    Imgs

--- 3_AVANCANDO/src/App.jsx
+++ 3_AVANCANDO/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Invencible from './assets/invencivel2.jpg'
 
 function App() {
   return (
@@ -6,6 +7,9 @@ function App() {
       <h1>Avançando em react</h1>
       <div>
         <img src="/invencivel.jpg" alt="Invencible" />{/* Acessando imagens no public */}
+      </div>
+      <div>
+        <img src={Invencible} alt="" style={{width: "300px"}}/>
       </div>
     </>
   )
