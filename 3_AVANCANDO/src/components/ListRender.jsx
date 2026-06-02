f1d4670f307f4e846a24c20402b56bb832f56d5e
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-06-02 14:39:21 -0300

    Listagem com ids

--- 3_AVANCANDO/src/components/ListRender.jsx
+++ 3_AVANCANDO/src/components/ListRender.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react"
 
 const ListRender = () => {
-    const [list] = useState(["Arthur", "Matheus", "Davi", "Caua"])
+    const [list] = useState([
+        {id: 1,name:"Arthur", age: 19}, 
+        {id: 2, name: "Matheus", age: 19}, 
+        {id: 3, name: "Davi", age: 27}, 
+        {id: 4,name: "Caua", age: 20}])
     return (
         <div>
             <h1>Listando</h1>
             <ul>
                 {list.map((item) => (
-                    <li>{item}</li>
+                    <li key={item.id}>{item.name} - {item.age}</li>
                 ))}
             </ul>
         </div>
