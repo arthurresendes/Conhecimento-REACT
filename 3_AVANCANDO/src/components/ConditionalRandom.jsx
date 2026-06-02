3042fe4405c8db1557242c7e52138f8c92c4ba79
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-06-02 14:57:28 -0300

    If e else no react

--- 3_AVANCANDO/src/components/ConditionalRandom.jsx
+++ 3_AVANCANDO/src/components/ConditionalRandom.jsx
@@ -2,6 +2,8 @@ import { useState } from "react"
 
 const ConditionalRandom = () => {
     const [x, setX] = useState(true)
+    const [name, setName] = useState("Arthur")
+
     const trocar = () =>{
         if(x){
             setX(false)
@@ -9,11 +11,23 @@ const ConditionalRandom = () => {
             setX(true)
         }
     }
+
+    const trocaNome = () =>{
+        if(name === "Arthur"){
+            setName("Matheus")
+        }else{
+            setName("Arthur")
+        }
+    }
+
     return (
         <div>
             <h1>Isso será exibido ?</h1>
             {x && <p>Sim</p>}
             <button onClick={trocar}>Trocar o status do exibir</button>
+
+            {name === "Arthur" ? (<p>Nome é Arthur</p>) : (<p>Nome não identificado</p>)}
+            <button onClick={trocaNome}>Trocar o nome</button>
         </div>
     )
 }
