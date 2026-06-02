8f554ce0d0048b34f0998869fc7cb75a38dd39c9
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-06-02 14:07:32 -0300

    Imgs

---
+++ 3_AVANCANDO/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Avançando em react</h1>
+      <div>
+        <img src="/invencivel.jpg" alt="Invencible" />{/* Acessando imagens no public */}
+      </div>
+    </>
+  )
+}
+
+export default App
