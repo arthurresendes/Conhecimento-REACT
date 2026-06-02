84c506fce2006f0fb61dc92ded3d2244fad84aa3
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-06-02 14:49:15 -0300

    Previ

--- 3_AVANCANDO/src/components/ListRender.jsx
+++ 3_AVANCANDO/src/components/ListRender.jsx
@@ -1,11 +1,20 @@
 import { useState } from "react"
 
 const ListRender = () => {
-    const [list] = useState([
+    const [list, setList] = useState([
         {id: 1,name:"Arthur", age: 19}, 
         {id: 2, name: "Matheus", age: 19}, 
         {id: 3, name: "Davi", age: 27}, 
         {id: 4,name: "Caua", age: 20}])
+    
+    const deleteRandom = () => {
+        const tam = list.length 
+        const num = Math.floor(Math.random() *tam)
+        console.log(num)
+        setList((prevUsers) => {
+            return prevUsers.filter((item => num !== item.id))
+        })
+    }
     return (
         <div>
             <h1>Listando</h1>
@@ -14,6 +23,7 @@ const ListRender = () => {
                     <li key={item.id}>{item.name} - {item.age}</li>
                 ))}
             </ul>
+            <button onClick={deleteRandom}>Delete random user</button>
         </div>
     )
 }
