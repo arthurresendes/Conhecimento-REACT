2616cf806a07b068bab8c22ea9a576e98db1ccb2
Arthur Resende Gomes <arthur.resende.gomes02@gmail.com>
2026-06-06 13:21:27 -0300

    CSS inline

--- 4_CSS_REACT/css/src/App.jsx
+++ 4_CSS_REACT/css/src/App.jsx
@@ -4,8 +4,11 @@ import MyComponent from './components/MyComponent'
 function App() {
   return (
     <>
-      <h1 style={{ textAlign: "center" }}>React com CSS</h1>
+      <h1>React com CSS</h1>
       <MyComponent />
+      <hr />
+      <h1>Inline</h1>
+      <p style={{ textAlign: "center", color: 'green', border: '1px solid black', borderRadius: '50px' }}>Paragrafo do app.js</p>
     </>
   )
 }
