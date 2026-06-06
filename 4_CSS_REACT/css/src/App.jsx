d31f70d3afe63a5b2ffec565527b7b9423ad5263
Arthur Resende Gomes <arthur.resende.gomes02@gmail.com>
2026-06-06 13:32:35 -0300

    CSS dinamico

--- 4_CSS_REACT/css/src/App.jsx
+++ 4_CSS_REACT/css/src/App.jsx
@@ -1,7 +1,10 @@
+import { useState } from 'react'
 import './App.css'
 import MyComponent from './components/MyComponent'
 
 function App() {
+  const n = 19
+  const [name, setName] = useState("")
   return (
     <>
       <h1>React com CSS</h1>
@@ -9,6 +12,16 @@ function App() {
       <hr />
       <h1>Inline</h1>
       <p style={{ textAlign: "center", color: 'green', border: '1px solid black', borderRadius: '50px' }}>Paragrafo do app.js</p>
+
+      <hr />
+      <h1 style={n > 10 ? ({ color: 'purple' }) : ({ color: 'orange' })}>CSS dinamico</h1>
+      <br />
+      <div style={{ textAlign: 'center' }}>
+        <label htmlFor="">Digite seu nome</label>
+        <br />
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        <p style={name === "Arthur" ? ({ color: 'blue' }) : ({ color: 'red' })}>Nome é: {name}</p>
+      </div>
     </>
   )
 }
