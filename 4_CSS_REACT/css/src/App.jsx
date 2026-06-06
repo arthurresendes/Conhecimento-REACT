804ef6b98baaf3167edc564f14908d6dde7e06de
Arthur Resende Gomes <arthur.resende.gomes02@gmail.com>
2026-06-06 13:18:23 -0300

    CSS de componente

--- 4_CSS_REACT/css/src/App.jsx
+++ 4_CSS_REACT/css/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import MyComponent from './components/MyComponent'
 
 function App() {
   return (
     <>
-      <h1 style={{textAlign: "center"}}>React com CSS</h1>
+      <h1 style={{ textAlign: "center" }}>React com CSS</h1>
+      <MyComponent />
     </>
   )
 }
---
+++ 4_CSS_REACT/css/src/components/MyComponent.jsx
@@ -0,0 +1,12 @@
+import "./componnet.css"
+
+const MyComponent = () => {
+  return (
+    <div>
+      <h1>CSS</h1>
+      <p className="my-comp-p">Paragrafo do componente</p>
+    </div>
+  );
+};
+
+export default MyComponent;
