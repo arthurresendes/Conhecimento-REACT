16cebd42c63b380b95bc786cb00ff088f8e2c49b
Arthur Resende Gomes <arthur.resende.gomes02@gmail.com>
2026-06-08 23:12:58 -0300

    Imagens

--- 3_AVANCANDO/src/App.jsx
+++ 3_AVANCANDO/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Invencible from './assets/invencivel2.jpg'
+{/*import Invencible from '../public/invencivel1.jpg' */ }
 import ManageData from './components/ManageData'
 import ListRender from './components/ListRender'
 import ConditionalRandom from './components/ConditionalRandom'
@@ -13,19 +13,37 @@ import ChangeMessagState from './components/ChangeMessagState'
 import UserDetails from './components/UserDetails'
 
 function App() {
+  const [indice, setIndice] = useState(1)
   const cars = [
-    {id: 1, brand: "Ferrari", color: "azul", novo: true, km: 0},
-    {id: 2, brand: "Ferrari", color: "vermelho", novo: false, km: 10000},
-    {id: 3, brand: "Ferrari", color: "verde", novo: true, km: 0},
+    { id: 1, brand: "Ferrari", color: "azul", novo: true, km: 0 },
+    { id: 2, brand: "Ferrari", color: "vermelho", novo: false, km: 10000 },
+    { id: 3, brand: "Ferrari", color: "verde", novo: true, km: 0 },
   ]
 
   const persons = [
-    {id: 1, name: "Arthur", age: 19, work: "Developer"},
-    {id: 2, name: "Matheus", age: 20, work: "Infra"},
-    {id: 3, name: "Arthur", age: 17, work: "Atendente"},
+    { id: 1, name: "Arthur", age: 19, work: "Developer" },
+    { id: 2, name: "Matheus", age: 20, work: "Infra" },
+    { id: 3, name: "Arthur", age: 17, work: "Atendente" },
   ]
 
-  function showMessage(){
+  const voltar = () => {
+    if (indice === 1) {
+      setIndice(3)
+    } else {
+      setIndice(indice - 1)
+    }
+  }
+
+
+  const proximo = () => {
+    if (indice === 3) {
+      setIndice(1)
+    } else {
+      setIndice(indice + 1)
+    }
+  }
+
+  function showMessage() {
     console.log("Minha função")
   }
 
@@ -38,30 +56,34 @@ function App() {
   return (
     <>
       <h1>Avançando em react</h1>
-      <div>
-        <img src="/invencivel.jpg" alt="Invencible" />{/* Acessando imagens no public */}
+      <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+        <button onClick={voltar} className='buttonImg' style={{ color: 'red' }}> &lt; </button>
+        <img src={`/invencivel${indice}.jpg`} alt="" style={{ width: "300px", height: "200px" }} />
+        <button onClick={proximo} className='buttonImg' style={{ color: 'green' }}> &gt; </button>
       </div>
+      {/*
       <div>
-        <img src={Invencible} alt="" style={{width: "300px"}}/>
+        <img src={Invencible} alt="" />
       </div>
-      <ManageData/>
-      <ListRender/>
-      <ConditionalRandom/>
-      <ShowUserName name="Arthur"/>{/*Pode passar tanto constantes sem e com useState como parametros nesse caso tambem*/}
-      <CarDetails brand="VW" km={0} color="azul" novo={true}/>
-      <CarDetails brand="VW" km={10000} color="azul" novo={false}/>
-      <CarDetails brand="VW" km={0} color="vermelho" novo={true}/>
-      {cars.map((car) =>(
-        <CarDetails key={car.id} brand={car.brand} km={car.km} color={car.color} novo={car.novo}/>
+      */}
+      <ManageData />
+      <ListRender />
+      <ConditionalRandom />
+      <ShowUserName name="Arthur" />{/*Pode passar tanto constantes sem e com useState como parametros nesse caso tambem*/}
+      <CarDetails brand="VW" km={0} color="azul" novo={true} />
+      <CarDetails brand="VW" km={10000} color="azul" novo={false} />
+      <CarDetails brand="VW" km={0} color="vermelho" novo={true} />
+      {cars.map((car) => (
+        <CarDetails key={car.id} brand={car.brand} km={car.km} color={car.color} novo={car.novo} />
       ))}
       <Container myValue="teste">
         <p>Componente filho</p>
       </Container>
-      <ExecuteFunction myFunction={showMessage}/>
-      <Message msg={message}/>
-      <ChangeMessagState handleMessage={handleMessageIntern}/>
+      <ExecuteFunction myFunction={showMessage} />
+      <Message msg={message} />
+      <ChangeMessagState handleMessage={handleMessageIntern} />
       {persons.map((person) => (
-        <UserDetails key={person.id} name={person.name} age={person.age} work={person.work}/>
+        <UserDetails key={person.id} name={person.name} age={person.age} work={person.work} />
       ))}
     </>
   )
