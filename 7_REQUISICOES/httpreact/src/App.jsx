60bc4667d7f7bd75e660a2e7c4b88f8318499a1f
Arthur Resende Gomes <arthur.resende.gomes02@gmail.com>
2026-06-23 16:10:17 -0300

    Fetch

--- 7_REQUISICOES/httpreact/src/App.jsx
+++ 7_REQUISICOES/httpreact/src/App.jsx
@@ -1,9 +1,25 @@
+import { useState, useEffect } from "react"
 
-
+const url = "http://localhost:3000/products/"
 function App() {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(url)
+      const data = await response.json()
+      setProducts(data)
+    }
+    fetchData()
+  }, [])
   return (
     <>
       <h1>Lista de produtos</h1>
+      <ul>
+        {products.map((prod) => (
+          <li key={prod.id}>{prod.name} - R$: {prod.price}</li>
+        ))}
+      </ul>
     </>
   )
 }
