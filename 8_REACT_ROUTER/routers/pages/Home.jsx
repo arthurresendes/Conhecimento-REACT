2b522cae9282d1ce706e27a113ce1e462db32886
Arthur Resende Gomes <arthur.resende.gomes02@gmail.com>
2026-06-30 17:57:49 -0300

    Rotas

--- 8_REACT_ROUTER/routers/pages/Home.jsx
+++ 8_REACT_ROUTER/routers/pages/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { useFetch } from '../hooks/useFetch'
 
 const Home = () => {
+    const url = 'http://localhost:3000/products'
+    const { data: items, httpConfig } = useFetch(url)
     return (
         <div>
-            <h2>Rota home</h2>
+            <h1>Produtos</h1>
+            <ul>
+                {items && items.map((item) => (
+                    <li key={item.id}>{item.name} - {item.price}</li>
+                ))}
+            </ul>
         </div>
     )
 }
