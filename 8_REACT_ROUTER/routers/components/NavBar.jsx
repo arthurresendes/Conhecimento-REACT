deb5ed18397318c671f05a414085df69159ec659
Arthur Resende Gomes <arthur.resende.gomes02@gmail.com>
2026-06-30 17:46:58 -0300

    Rotas

---
+++ 8_REACT_ROUTER/routers/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './NavBar.css'
+
+const NavBar = () => {
+    return (
+        <nav>
+            <Link to='/'>Home</Link>
+            <Link to='about'>Sobre</Link>
+        </nav>
+    )
+}
+
+export default NavBar
--- 8_REACT_ROUTER/routers/src/App.jsx
+++ 8_REACT_ROUTER/routers/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from '../components/Home'
-import About from '../components/About'
+import Home from '../pages/Home'
+import About from '../pages/About'
+import NavBar from '../components/NavBar'
 
 function App() {
   return (
     <div style={{ textAlign: 'center' }}>
       <h1>React Router</h1>
       <BrowserRouter>
+        <NavBar />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
