c5f5751ad868af043c1ab9bd22af268a7823ec5f
Arthur Resende Gomes <arthur.resende.gomes02@gmail.com>
2026-07-04 13:32:20 -0300

    NotFound + isActive

--- 8_REACT_ROUTER/routers/components/NavBar.jsx
+++ 8_REACT_ROUTER/routers/components/NavBar.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import './NavBar.css'
 
 const NavBar = () => {
     return (
         <nav>
-            <Link to='/'>Home</Link>
+            {/*<Link to='/'>Home</Link>
             <Link to='sobre'>Sobre</Link>
+            */}
+            <NavLink to="/" className={ }>Home</NavLink>
+            <NavLink to="/about">Sobre</NavLink>
         </nav>
     )
 }
--- 8_REACT_ROUTER/routers/src/App.jsx
+++ 8_REACT_ROUTER/routers/src/App.jsx
@@ -4,6 +4,7 @@ import About from '../pages/About'
 import NavBar from '../components/NavBar'
 import Product from '../pages/Product'
 import Info from '../pages/Info'
+import NotFound from '../pages/NotFound'
 
 function App() {
   return (
@@ -13,9 +14,10 @@ function App() {
         <NavBar />
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/sobre' element={<About />} />
+          <Route path='/about' element={<About />} />
           <Route path='/products/:id/info' element={<Info />} />
           <Route path='/products/:id' element={<Product />} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
