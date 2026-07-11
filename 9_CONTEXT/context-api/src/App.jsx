5e6bdf05a5b0be4adb49bd7323d59a5ef42ff684
Arthur Resende Gomes <arthur.resende.gomes02@gmail.com>
2026-07-11 12:54:00 -0300

    Inicializando

---
+++ 9_CONTEXT/context-api/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import NavBar from './components/NavBar'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+
+function App() {
+  return (
+    <>
+      <h1>Rotas</h1>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
